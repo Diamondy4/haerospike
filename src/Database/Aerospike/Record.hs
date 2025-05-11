2f62b080a2d93e666e4c8a5c25920cd7f9709297
Ilya Shpilkov <ilya.shpilkov@mail.ru>
2025-05-11 23:57:34 +0300

    chore: add generic convertions between raw bins and haskell types

--- src/Database/Aerospike/Record.hs
+++ src/Database/Aerospike/Record.hs
@@ -1,10 +1,43 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Database.Aerospike.Record where
 
-import Data.ByteString qualified as BS
+import Data.ByteString qualified as BS hiding (pack)
+import Data.ByteString.Char8 qualified as BS
+import Data.Map qualified as M
 import Data.Word (Word16, Word32)
-import Database.Aerospike.Value
+import Database.Aerospike.Value (
+    FromValue (..),
+    ToValue (..),
+    Value,
+ )
+
+import Generics.SOP (
+    All,
+    ConstructorInfo (Record),
+    DatatypeInfo (ADT, Newtype),
+    FieldInfo (..),
+    Generic (..),
+    HCollapse (hcollapse),
+    HasDatatypeInfo (datatypeInfo),
+    I (..),
+    K (..),
+    NP (Nil, (:*)),
+    NS (Z),
+    Proxy (Proxy),
+    SOP (SOP),
+    unSOP,
+    unZ,
+ )
+import Generics.SOP.NP (cmap_NP, czipWith_NP, sequence_NP)
 
 data Record = MkRecord
     { gen :: Word16
@@ -14,3 +47,77 @@ data Record = MkRecord
     , bins :: [(BS.ByteString, Value)]
     }
     deriving stock (Show, Eq, Ord)
+
+class FromAsBins a where
+    fromAsBins :: [(BS.ByteString, Value)] -> Maybe a
+    default fromAsBins ::
+        ( Generic a
+        , HasDatatypeInfo a
+        , Code a ~ '[xs]
+        , All FromValue xs
+        ) =>
+        [(BS.ByteString, Value)] ->
+        Maybe a
+    fromAsBins = gFromAsBins
+
+class ToAsBins a where
+    toAsBins :: a -> [(BS.ByteString, Value)]
+    default toAsBins ::
+        ( Generic a
+        , HasDatatypeInfo a
+        , Code a ~ '[xs]
+        , All ToValue xs
+        ) =>
+        a ->
+        [(BS.ByteString, Value)]
+    toAsBins = gToAsBins
+
+gToAsBins ::
+    forall a xs.
+    ( Generic a
+    , HasDatatypeInfo a
+    , Code a ~ '[xs]
+    , All ToValue xs
+    ) =>
+    a ->
+    [(BS.ByteString, Value)]
+gToAsBins r =
+    let
+        fields = unZ . unSOP . from $ r
+        fieldsInfo = datatypeInfo (Proxy @a)
+     in
+        case fieldsInfo of
+            ADT _ _ (Record _ fieldNames :* Nil) _ -> hcollapse $ czipWith_NP (Proxy @ToValue) mkBin fieldNames fields
+            Newtype _ _ (Record _ name) -> hcollapse $ czipWith_NP (Proxy @ToValue) mkBin name fields
+            _ -> error "try to serialize unsupported data type"
+  where
+    mkBin :: forall t. (ToValue t) => FieldInfo t -> I t -> K (BS.ByteString, Value) t
+    mkBin (FieldInfo name) (I x) = K (BS.pack name, toValue x)
+
+gFromAsBins ::
+    forall a xs.
+    ( Generic a
+    , HasDatatypeInfo a
+    , Code a ~ '[xs]
+    , All FromValue xs
+    ) =>
+    [(BS.ByteString, Value)] ->
+    Maybe a
+gFromAsBins bins =
+    let
+        mp = M.fromList bins
+        lookup name = mp M.!? BS.pack name
+
+        extractField :: forall t. (FromValue t) => FieldInfo t -> Maybe t
+        extractField (FieldInfo name) = do
+            value <- lookup name
+            fromValue value
+     in
+        case datatypeInfo (Proxy @a) of
+            ADT _ _ (Record _ fieldNames :* Nil) _ -> do
+                fields <- sequence_NP $ cmap_NP (Proxy @FromValue) extractField fieldNames
+                pure $ to (SOP (Z fields))
+            Newtype _ _ (Record _ name) -> do
+                fields <- sequence_NP $ cmap_NP (Proxy @FromValue) extractField name
+                pure $ to (SOP (Z fields))
+            _ -> error "try to serialize unsupported data type"
--- src/Database/Aerospike/Value.hs
+++ src/Database/Aerospike/Value.hs
@@ -1,7 +1,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Database.Aerospike.Value where
 
+import Data.Bifunctor (Bifunctor (..))
 import Data.ByteString qualified as BS
 import Data.Int (Int64)
 import Data.Map.Strict qualified as Map
@@ -21,3 +23,61 @@ data Value
 
 class FromValue a where
     fromValue :: Value -> Maybe a
+
+instance FromValue Bool where
+    fromValue = \case
+        VBoolean v -> Just v
+        _ -> Nothing
+
+instance FromValue Int64 where
+    fromValue = \case
+        VInteger v -> Just v
+        _ -> Nothing
+
+instance FromValue Text where
+    fromValue = \case
+        VString v -> Just v
+        _ -> Nothing
+
+instance (FromValue a) => FromValue (V.Vector a) where
+    fromValue = \case
+        VList v -> V.forM v fromValue
+        _ -> Nothing
+
+instance (Ord k, FromValue k, FromValue v) => FromValue (Map.Map k v) where
+    fromValue = \case
+        VMap v -> do
+            let parsePair (k, v) = do
+                    k' <- fromValue @k k
+                    v' <- fromValue @v v
+                    pure (k', v')
+
+            pairs <- traverse parsePair . Map.assocs $ v
+            pure $ Map.fromList pairs
+        _ -> Nothing
+
+instance FromValue BS.ByteString where
+    fromValue = \case
+        VBytes v -> Just v
+        _ -> Nothing
+
+class ToValue a where
+    toValue :: a -> Value
+
+instance ToValue Bool where
+    toValue = VBoolean
+
+instance ToValue Int64 where
+    toValue = VInteger
+
+instance ToValue Text where
+    toValue = VString
+
+instance (ToValue a) => ToValue (V.Vector a) where
+    toValue = VList . V.map toValue
+
+instance (ToValue k, ToValue v) => ToValue (Map.Map k v) where
+    toValue = VMap . Map.fromList . fmap (bimap toValue toValue) . Map.assocs
+
+instance ToValue BS.ByteString where
+    toValue = VBytes
